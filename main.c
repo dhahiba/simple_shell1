9b0a18d2862590a0d955aaba52baee4e28c18df8
dhahiba <dhhiba1991@gmail.com>
2023-05-24 10:10:57 -0700

    tt

--- main.c
+++ main.c
@@ -13,22 +13,37 @@ int s = 0;
 
 int main(int argc __attribute__((unused)), char **argv)
 {
-char command[MAX_COMMAND_LENGTH];
-char *arguments[MAX_ARGUMENTS];
-int arg_count;
+char **c = NULL;
+int i, tc = 0;
+size_t n = 0;
 
 signal(SIGINT, ctrlhandler);
 sn = argv[0];
 while (1)
 {
-printf(":) ");
-fgets(command, sizeof(command), stdin);
-
-arg_count = tokenize_command(command, arguments);
-if (arg_count > 0)
+handle_mode();
+_print(" ($) ", STDOUT_FILENO);
+if (getline(&l, &n, stdin) == -1)
 {
-execute_command(arguments);
+free(l);
+exit(s);
 }
+r_line(l);
+r_comment(l);
+cmd = _tokenizer(l, ";");
+for (i = 0; cmd[i] != NULL; i++)
+{
+c = _tokenizer(cmd[i], " ");
+if (c[0] == NULL)
+{
+free(c);
+break;
+}
+tc = p_command(c[0]);
+start(c, tc);
+free(c);
+}
+free(cmd);
 }
 free(l);
 return (s);
